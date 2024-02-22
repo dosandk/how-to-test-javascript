7343deaf87798ea24678e326ed22684e41147196
Volodymyr Shevchuk <dosandkv@gmail.com>
2024-02-23 01:06:54 +0200

    update range-picker tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,5 @@
 module.exports = function(config) {
   config.set({
-
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '', // relative to __dirname
 
@@ -8,22 +7,21 @@ module.exports = function(config) {
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
-
     // list of files / patterns to load in the browser
     files: [
-      { pattern: 'src/range-picker/index.spec.js', type: 'module', included: true, served: true },
+      {
+        pattern: 'src/range-picker/index.spec.js',
+        type: 'module',
+        included: true,
+        served: true
+      },
       { pattern: '**/*.js', type: 'module', included: false, served: true },
       { pattern: '**/*.css', type: 'css', included: true, served: true },
-      { pattern: '**/*.svg', included: false, served: true },
+      { pattern: '**/*.svg', included: false, served: true }
     ],
-
 
     // list of files to exclude
-    exclude: [
-      '*module/*task/modules/**',
-      '**/webpack.config.js'
-    ],
-
+    exclude: ['*module/*task/modules/**', '**/webpack.config.js'],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
@@ -35,16 +33,15 @@ module.exports = function(config) {
     reporters: ['progress'], // progress
 
     jsonReporter: {
-      stdout: true,
+      stdout: false
       // outputFile: 'results/results.json' // defaults to none
     },
-
 
     // web server port
     port: 9876,
 
     // enable / disable colors in the output (reporters and logs)
-    colors: false,
+    colors: true,
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
@@ -55,15 +52,14 @@ module.exports = function(config) {
       terminal: false
     },
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    // browsers: ["Chrome"],
     // browsers: ['Firefox'],
-    // browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadless'],
     // browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
@@ -72,14 +68,14 @@ module.exports = function(config) {
           '--no-sandbox',
           '--user-data-dir=/tmp/chrome-test-profile',
           '--disable-web-security',
-          '--lang=ru' // affects date.toLocaleString formatting for tests
+          '--lang=en' // affects date.toLocaleString formatting for tests
         ]
       }
     },
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
